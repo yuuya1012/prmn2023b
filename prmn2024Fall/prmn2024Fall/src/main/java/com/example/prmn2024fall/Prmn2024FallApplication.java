2276ef4e4ed55624be430f2f3b3f6a52694ac51c
yuuya1012 <yuuya020202@icloud.com>
2024-10-24 12:52:17 +0900

    Github上に表示するコメント文(例:プロジェクト作成)

---
+++ prmn2024Fall/prmn2024Fall/src/main/java/com/example/prmn2024fall/Prmn2024FallApplication.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024fall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2024FallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024FallApplication.class, args);
+    }
+
+}
---
+++ prmn2024Fall/prmn2024Fall/src/test/java/com/example/prmn2024fall/Prmn2024FallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2024fall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024FallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
